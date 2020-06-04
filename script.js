6fbf527d8576e4d6a351b15e3aa6267b4b3deccc
m-alcu <martin.alcubierre@gmail.com>
2020-06-04 15:37:59 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const video = document.getElementById('video')
+const MASK_CLASSES = ['Mask', 'No Mask'];
+let model
+
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+  loadfacemaskModel('./model/model.json')
+]).then(startVideo)
+
+async function loadfacemaskModel(urlModel) {
+  model = await tf.loadLayersModel(urlModel);
+}
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} }, stream => video.srcObject = stream, err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video)
+    const faceImages = await faceapi.extractFaces(video, detections)
+    const faceTensors = tf.tidy(() => {
+      return faceImages.map(canvas => tf.browser.fromPixels(canvas)
+      .resizeNearestNeighbor([224, 224])
+      .toFloat()
+      .reverse(2)
+      .expandDims()
+      .div(127.5)
+      .sub(1))
+    });
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    faceTensors.forEach((faceTensor, i) => {
+      const facemaskPrediction = model.predict(faceTensor)
+      const maskLabel = MASK_CLASSES[facemaskPrediction.argMax(-1).dataSync()[0]];
+      const faceBox = resizedDetections[i].box
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+      const drawBox = new faceapi.draw.DrawBox(faceBox, { label: maskLabel.toString() , boxColor: maskLabel == MASK_CLASSES[0] ? 'blue' : 'red'})
+      drawBox.draw(canvas)
+      faceTensor.dispose()
+      facemaskPrediction.dispose()
+    })
+  }, 200)
+})
